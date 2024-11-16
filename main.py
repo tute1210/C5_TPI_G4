e051d57497732b00042bf710b0086a42caa2c78e
efracruzz <cruzefra70@gmail.com>
2024-11-15 22:24:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from juegos.CazadoresDeGramatica import test (Linea de codigo definida para cuando esten las funciones echas)
 from juegos.JuegoAdivinaAnimal import iniciar_juego
 from juegos.ChOrD3 import juego_musica
+from juegos.LlamarJuegoMultiplicar import tablas
 menu = 0
 
 while menu != 3:
@@ -13,17 +14,20 @@ while menu != 3:
     print()
     print("        |1| →   Cazadores De Gramatica    |")
     print("        |2| →       Adivina El Animal     |")
+    print("        |3| →   Tablas de multiplicar     |")
     print("        |4| →            Ch0rD3           |")
-    print("        |3| →            SALIR            |")
+    print("        |5| →            SALIR            |")
     print()
     menu = int(input("              Opcion a elegir →"))
     if menu == 1:
         from juegos.CazadoresDeGramatica import test #IGNORAR ESTA LINEA
     elif menu == 2:
         iniciar_juego()
+    elif menu == 3:
+        tablas()
     elif menu == 4:
         juego_musica()
-    elif menu == 3:
+    elif menu == 5:
         print("            PROGRAMA FINALIZADO")
 
         
