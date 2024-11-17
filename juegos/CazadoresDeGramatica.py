62affdedad34aa653189262b6ea1256107718616
matiassfunes <matiassfunes@gmail.com>
2024-11-17 13:48:07 -0300

    a

--- juegos/CazadoresDeGramatica.py
+++ juegos/CazadoresDeGramatica.py
@@ -2,6 +2,9 @@
 def cazadores_de_gramatica():
     from juegos.utilidades.funciones_CDG import leer_archivo
     from juegos.utilidades.funciones_CDG import comparacion_oraciones
+    from juegos.utilidades.funciones_CDG import comparacion_sustantivos
+    from juegos.utilidades.funciones_CDG import comparacion_verbos
+    from juegos.utilidades.funciones_CDG import comparacion_adjetivos
     from juegos.utilidades.inicio_CDG import incio_CDG
     import random # Libreria "random" para poder obtener oraciones aleatorias de la lista "oraciones"
 
@@ -92,81 +95,3 @@ def cazadores_de_gramatica():
         if volver_menu == 'x':
             print("Regresando al menú principal...")
             break
-
-    #Función que compara la oración correcta guardada en oraciones.txt con lo que ingresa el jugador
-    # def comparacion_oraciones(oracion_correcta, oracion_player):
-    #     palabras_correctas = oracion_correcta.split()
-    #     palabras_jugador = oracion_player.split()
-
-    #     aciertos = 0
-    #     errores = 0
-
-    #     long = len(palabras_correctas)
-    #     # Compara palabra por palabra
-
-    #     for palabra in palabras_jugador:
-    #         if palabra in palabras_correctas:
-    #             print(f"{GREEN} {palabra} {END}", end = "")
-    #         else:
-    #             errores += 1
-    #             print(f"{RED} {palabra} {END}", end = "")
-
-    #     return errores
-
-    #Función que compara los sustantivos correctos guardados en oraciones.txt con lo que ingresa el jugador
-    def comparacion_sustantivos(sustantivos, sustantivos_jugador):
-        sustantivos_correctos = sustantivos.split()
-        sustantivos_jugador = sustantivos_jugador.split()
-
-        aciertos = 0
-        errores = 0
-
-        long = len(sustantivos_correctos)
-
-        for sustantivo in sustantivos_jugador:
-            if sustantivo in sustantivos_correctos:
-                aciertos += 1
-            else:
-                errores += 1
-        # print("Aciertos: ", aciertos)
-        # print("Errores: ", errores)
-        return errores
-    
-    #Función que compara los verbos correctos guardados en oraciones.txt con lo que ingresa el jugador
-    def comparacion_verbos(verbos, verbos_jugador):
-        verbos_correctos = verbos.split()
-        verbos_jugador = verbos_jugador.split()
-
-        aciertos = 0
-        errores = 0
-
-        long = len(verbos_correctos)
-
-        for verbo in verbos_jugador:
-            if verbo in verbos_correctos:
-                aciertos += 1
-            else:
-                errores += 1
-        # print("Aciertos: ", aciertos)
-        # print("Errores: ", errores)
-        return errores
-    
-    #Función que compara los adjetivos correctos guardados en oraciones.txt con lo que ingresa el jugador
-    def comparacion_adjetivos(adjetivos, adjetivos_jugador):
-        adjetivos_correctos = adjetivos.split()
-        adjetivos_jugador = adjetivos_jugador.split()
-
-        aciertos = 0
-        errores = 0
-
-        long = len(adjetivos_correctos)
-
-        for adjetivo in adjetivos_jugador:
-            if adjetivo in adjetivos_correctos:
-                aciertos += 1
-            else:
-                errores += 1
-        
-        # print("Aciertos: ", aciertos)
-        # print("Errores: ", errores)
-        return errores
